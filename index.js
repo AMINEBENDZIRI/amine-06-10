14f96cc818808ff007c2175c623145cc183d9b9b
AMINEBENDZIRI <114341806+AMINEBENDZIRI@users.noreply.github.com>
2022-10-04 00:22:06 +0200

    firstcommit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const parkings = require('./parkings.json')
+app.get('/parkings', (req,res) => {    res.status(200).json(parkings)})
+
+
+
+app.listen(8080, () => {
+      console.log("Serveur à l'écoute")  
+    
+         })
